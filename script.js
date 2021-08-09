1f088b3aa874838e1924f689394b16f34f114344
Pratyush kiran Rout <77445957+kiranpratyush@users.noreply.github.com>
2021-08-09 17:38:43 +0530

    Fix:percentage profit and Loss

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function calcProfitOrLoss(costPrice, quantity, sellingPrice) {
   let percentloss;
   if (costPrice > sellingPrice) {
     loss = (costPrice - sellingPrice) * quantity;
-    percentloss = Math.round((loss / costPrice) * (100 * quantity));
+    percentloss = Math.round((loss / (costPrice * quantity)) * (100));
     console.log(percentloss);
     if (percentloss > 50) {
       h3.textContent = `You have Made total ${loss} rupees loss(in ${percentloss}%`;
@@ -25,7 +25,7 @@ function calcProfitOrLoss(costPrice, quantity, sellingPrice) {
     }
   } else if (sellingPrice > costPrice) {
     profit = (sellingPrice - costPrice) * quantity;
-    percentProfit = Math.round((profit / costPrice) * 100 * quantity);
+    percentProfit = Math.round((profit / (costPrice * quantity)) * 100);
     h3.textContent = `You have Made total ${profit} rupees profit(in ${percentProfit}%)`;
     item2.style.backgroundColor = "green";
   } else {
